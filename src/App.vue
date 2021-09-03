210fe7b1cedd37a7c335a0bf35e2a2e8339d3458
Dulun <dulunix@gmail.com>
2021-09-03 10:47:18 +0800

    separate html css

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- 拆，先拆结构，再拆样式 -->
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <VHeader></VHeader>
+        <VList></VList>
+        <VFooter></VFooter>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VHeader from './components/VHeader'
+import VFooter from './components/VFooter'
+import VList from './components/VList'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    VHeader,
+    VFooter,
+    VList,
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/*base*/
+body {
+  background: #fff;
 }
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
+
+
+
+
+
 </style>
---
+++ src/components/VList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="todo-main">
+    <VItem></VItem>
+  </ul>
+</template>
+
+<script>
+import VItem from './VItem'
+export default {
+  name: 'VList',
+  components: { VItem },
+}
+</script>
+
+<style scoped>
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
